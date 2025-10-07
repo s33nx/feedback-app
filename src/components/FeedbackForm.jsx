09ba3e73b326d682488168183169b15136294696
s33nx <muhseenibrahim5@gmail.com>
2025-10-07 17:44:31 +0100

    custon button component and real time validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { useState } from 'react'
 import Card from './shared/Card'
-
+import Button from './shared/Button'
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState('true')
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        
+        if(message === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        }else if(text !== '' && text.trim().length <= 10){
+            setBtnDisabled(true)
+            setMessage('Text must be at least 10 characters')
+        }else{
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setText(e.target.value)
     }
   return (
@@ -16,8 +29,9 @@ function FeedbackForm() {
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" placeholder='Write a review' 
                 value={text}/>
-                <button type="submit">Send</button>
+                <Button type="submit" isDisabled={btnDisabled} >Send</Button>
             </div>
+            {message && <div className='message'>{message}</div>}
         </form>
     </Card>
   )
